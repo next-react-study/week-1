e3a59b87342bbdd84f8cbb7c14078cfdbba1feb2
amorfati0310 <lyhy0310@gmail.com>
2020-07-04 13:49:54 +0900

    feat: 기본 route setup
    [Ticket: 2]

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Home from './pages/Home'
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Route path="/" component={Home} exact />
     </div>
   );
 }
